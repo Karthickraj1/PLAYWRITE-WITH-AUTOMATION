04227ef0b887bd42b64a929171ca0d0c4c882391
Sathish Kumar <sathishkumarms04@gmail.com>
2026-04-10 15:41:38 +0530

    h

---
+++ tests/findmax.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test'; 
+function findMax(...args: number[]): number {
+    let  max = args[0];
+    for (let num of args){
+        if (num > max){
+            max = num;
+        }   
+    }
+    return max;
+
+}
+console.log(findMax(3, 5, 1, 8, 2)); // Output: 8
+console.log(findMax(-1, -5, -3)); // Output: -1
